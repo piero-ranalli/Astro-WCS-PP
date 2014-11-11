f5d08fe96a466bcf5e7221376b2660519f597dbd
Piero Ranalli <piero@rinne.astro.noa.gr>
2014-11-12 01:51:38 +0200

    updated license information / 2

--- lib/Astro/WCS/PP.pm
+++ lib/Astro/WCS/PP.pm
@@ -1,5 +1,4 @@
-# the XMM-CDFS simulator of astronomical X-ray observations
-# Copyright (C) 2013 Piero Ranalli
+# Copyright (C) 2005-2014 Piero Ranalli
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU Affero General Public License as
